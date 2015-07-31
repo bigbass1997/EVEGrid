fbefe15e11074c34a332da2db4b8d54538a2ec9c
bigbass1997 <bigbass1997.website@gmail.com>
2015-07-30 22:08:03 -0500

    Init Commit

---
+++ core/src/com/bigbass1997/evegrid/Main.java
@@ -0,0 +1,18 @@
+package com.bigbass1997.evegrid;
+
+import com.badlogic.gdx.ApplicationAdapter;
+
+public class Main extends ApplicationAdapter {
+	
+	@Override
+	public void create(){
+	}
+
+	@Override
+	public void render(){
+	}
+	
+	@Override
+	public void dispose(){
+	}
+}
---
+++ desktop/src/com/bigbass1997/evegrid/desktop/DesktopLauncher.java
@@ -0,0 +1,16 @@
+package com.bigbass1997.evegrid.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.bigbass1997.evegrid.Main;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		
+		config.width = 800;
+		config.height = 600;
+		
+		new LwjglApplication(new Main(), config);
+	}
+}
