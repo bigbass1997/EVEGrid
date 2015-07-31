0a73c664cdc4e08603b0efccbb90e971623219b5
bigbass1997 <bigbass1997.website@gmail.com>
2015-07-31 14:56:30 -0500

    Added jcabi-http library

--- core/src/com/bigbass1997/evegrid/states/StateMainMenu.java
+++ core/src/com/bigbass1997/evegrid/states/StateMainMenu.java
@@ -1,16 +1,23 @@
 package com.bigbass1997.evegrid.states;
 
 import com.badlogic.gdx.graphics.glutils.ShapeRenderer.ShapeType;
+import com.badlogic.gdx.scenes.scene2d.ui.TextField;
 import com.bigbass1997.evegrid.graphics.Draw;
 
 public class StateMainMenu extends State {
 	
+	private TextField tField;
+	
 	public StateMainMenu(StateManager sm){
 		super(sm, "MAIN_MENU");
+		
+		//tField = new TextField();
 	}
 	
 	public void render(){
 		Draw.rect(sr, 50, 50, 100, 200, ShapeType.Filled, 0x00FF00FF);
+		
+		
 	}
 	
 	public void update(float delta){
